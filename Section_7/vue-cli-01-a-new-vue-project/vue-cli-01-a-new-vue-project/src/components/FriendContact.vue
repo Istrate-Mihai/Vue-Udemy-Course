93e7db7384b2038d796db927646c808f991e8ed6
Istrate Mihai Septimius <i.mihai9960@gmail.com>
2023-05-20 22:17:17 +0300

    Added section 7 of course

---
+++ Section_7/vue-cli-01-a-new-vue-project/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,34 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">
+      {‌{ detailsAreVisible ? 'Hide' : 'Show' }} details
+    </button>
+    <ul v-if="this.detailsAreDisplayed">
+      <li><strong>Phone:</strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'FriendContact',
+  data() {
+    return {
+      detailsAreDisplayed: true,
+      friend: {
+        id: 1,
+        name: 'Manuel Lorenz',
+        phone: '0123456789',
+        email: 'manuel@localhost.com',
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreDisplayed = !this.detailsAreDisplayed;
+    },
+  },
+};
+</script>
